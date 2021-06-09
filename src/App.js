e9e3f9c2d3d206092ab429b77f033bdd030427e6
meriemkhadraoui <miyakhad@gmail.com>
2021-06-09 13:14:15 +0200

    morse app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
-
 import Data from "./data.json";
-function App() {
+const App = () => {
+  const [translatedText, setTranslatedText] = useState("");
   const [userInput, setUserInput] = useState("");
-  const data = Data.map((prop) => console.log(prop));
+  function handleChange(e) {
+    setUserInput(e.target.value);
+  }
+  function codeThatText(e) {
+    e.preventDefault();
+    let morseText = "";
+    userInput.split("").forEach((chr) => {
+      morseText += " " + Data[chr.toLowerCase()];
+    });
+    setTranslatedText(morseText);
+  }
+
   return (
     <React.Fragment>
-      <Header userInput={setUserInput} />
+      <h3>Translate to Morse</h3>
+      <form>
+        <div className="container">
+          <label>Input Message:</label>
+          <textarea
+            cols="50"
+            rows="8"
+            onKeyUp={codeThatText}
+            onChange={handleChange}
+            placeholder="Type a message here"
+          />
+        </div>
+        <div className="container">
+          <label>Output Message:</label>
+          <textarea
+            cols="50"
+            rows="8"
+            placeholder="Here comes Morse code"
+            value={translatedText}
+            readOnly
+          />
+        </div>
+      </form>
     </React.Fragment>
   );
-}
+};
 
 export default App;
